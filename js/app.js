2fe9d1c71a2262580f61fe2e69d420eaec3221af
josePoblete96 <jose.poblete@outlook.cl>
2023-03-10 10:12:58 -0300

    conexion a fb

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
-const openModal = document.getElementById('openRegisterModal');
-const modal= document.getElementById('modal');
-const closeModal= document.getElementById('closeRegisterModal');
-
-const showRegisterModal=()=>{
-    modal.classList.toggle('is-active')
+const firebaseConfig = {
+    apiKey: "AIzaSyAri_MHTyNs1CaZ-V2VpPo_-xLrSiKsuY8",
+    authDomain: "sporstsmd.firebaseapp.com",
+    projectId: "sporstsmd",
+    storageBucket: "sporstsmd.appspot.com",
+    messagingSenderId: "985116999667",
+    appId: "1:985116999667:web:e3a4376f02418a3bc05c86",
+    measurementId: "G-VCMY8C4GJT"
 };
 
-openModal.addEventListener('click', showRegisterModal);
-closeModal.addEventListener('click', showRegisterModal);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = firebase.firestore();
